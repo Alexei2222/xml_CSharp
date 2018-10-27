0afe0711c22a2fb2f35dd11460f9efd36604a031
Sit <Sit@S31903>
2018-10-27 11:36:53 +0300

    Add ReadXml from file

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
+using System.Xml;
 
 namespace ConsoleApplication
 {
@@ -15,6 +13,7 @@ namespace ConsoleApplication
         {
             Console.WriteLine("XML");
             CreateXmlFile();
+            ReadXMLFile();
 
 
             Console.ReadKey();
@@ -33,6 +32,23 @@ namespace ConsoleApplication
             TextWriter FileStream = new StreamWriter(@"D:\Users.xml");
             serialiser.Serialize(FileStream, listUsers);
             FileStream.Close();
+        }
+        public static void ReadXMLFile() {
+            var xmldoc = new XmlDocument();
+            XmlNodeList xmlnode;
+            int i = 0;
+            string str = null;
+            FileStream fs = new FileStream(@"D:\Users.xml", FileMode.Open, FileAccess.Read);
+            xmldoc.Load(fs);
+            xmlnode = xmldoc.GetElementsByTagName("User");
+            for (i = 0; i <= xmlnode.Count - 1; i++) {
+                xmlnode[i].ChildNodes.Item(0).InnerText.Trim();
+                str = xmlnode[i].ChildNodes.Item(0).InnerText.Trim() + " " + xmlnode[i].ChildNodes.Item(1).InnerText.Trim() + " " + xmlnode[i].ChildNodes.Item(2).InnerText.Trim();
+                Console.WriteLine(str);
+            }
+            fs.Close();
+            Console.WriteLine("\n End Of File");
+            Console.ReadKey();
         }
     }
 }
